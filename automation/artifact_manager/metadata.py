f2809c6d2c17e76a2a1e7e4e65a1c1acae9f1001
premchandoffice <premchand@lyrainfo.com>
2026-07-13 13:54:42 +0530

    Add metadata management for generated artifacts
    This file maintains metadata for generated artifacts, including project details and status.

---
+++ automation/artifact_manager/metadata.py
@@ -0,0 +1,49 @@
+"""
+metadata.py
+
+Maintain metadata for generated artifacts.
+"""
+
+import json
+from pathlib import Path
+from datetime import datetime
+
+
+class Metadata:
+
+    def __init__(self):
+
+        self.metadata = Path("storage/metadata")
+
+        self.metadata.mkdir(parents=True, exist_ok=True)
+
+    def create(self,
+               project_name,
+               sbom,
+               cbom,
+               report,
+               status):
+
+        data = {
+
+            "project": project_name,
+
+            "status": status,
+
+            "generated_on": datetime.now().isoformat(),
+
+            "sbom": sbom,
+
+            "cbom": cbom,
+
+            "report": report
+
+        }
+
+        output = self.metadata / f"{project_name}.json"
+
+        with open(output, "w") as file:
+
+            json.dump(data, file, indent=4)
+
+        print(f"Metadata created -> {output}")
